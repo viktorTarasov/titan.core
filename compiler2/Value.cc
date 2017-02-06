94bb7768784c99cbc5a0d20c2e85c59770743263
BenceJanosSzabo <bence.janos.szabo@ericsson.com>
2017-02-06 14:05:41 +0100

    Modified incorrect index error message
    Change-Id: I40bdf6037734bfffe76e826547307517cf2e3fd6
    Signed-off-by: BenceJanosSzabo <bence.janos.szabo@ericsson.com>

--- compiler2/Value.cc
+++ compiler2/Value.cc
@@ -9110,7 +9110,7 @@ error:
         index = v_index->get_val_Int()->get_val();
         index_available = true;
       } else if (!silent) {
-        array_index->error("An integer value was expected as index");
+        array_index->error("An integer value or a fixed length array or record of integer value was expected as index");
       }
     }
     if (valuetype == V_OMIT) {
