36ef06851d130cc126a7e6aa36315a617c98a963
kristof <Kristof.Szabados@ericsson.com>
2017-05-25 09:14:16 +0200

    actually what we try to ask is: can we generate the code as a single expression passed as a parameter
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- compiler2/ttcn3/AST_ttcn3.cc
+++ compiler2/ttcn3/AST_ttcn3.cc
@@ -5563,12 +5563,11 @@ namespace Ttcn {
         "extern TIMER %s;\n", genname_str);
       if (default_duration) {
         // has default duration
-        Value *v = default_duration->get_value_refd_last();
-        if (v->get_valuetype() == Value::V_REAL) {
+        if (default_duration->has_single_expr()) {
           // duration is known at compilation time -> set in the constructor
           target->source.global_vars = mputprintf(target->source.global_vars,
             "TIMER %s(\"%s\", %s);\n", genname_str, dispname.c_str(),
-            v->get_single_expr().c_str());
+            default_duration->get_single_expr().c_str());
         } else {
           // duration is known only at runtime -> set in post_init
           target->source.global_vars = mputprintf(target->source.global_vars,
