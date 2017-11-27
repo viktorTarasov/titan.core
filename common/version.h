a05c9de0115803c0ec6434f1e88c6a9abde8c7d8
balaskoa <jeno.balasko@ericsson.com>
2017-11-27 09:41:30 +0100

    Prepared for release CNL 113 200/6 R3B
    Signed-off-by: balaskoa <jeno.balasko@ericsson.com>

--- common/version.h
+++ common/version.h
@@ -25,7 +25,7 @@
 /* Version numbers */
 #define TTCN3_MAJOR 6
 #define TTCN3_MINOR 3
-#define TTCN3_PATCHLEVEL 0   //0-> x=A, 1-> x=B, ... 
+#define TTCN3_PATCHLEVEL 1   //0-> x=A, 1-> x=B, ...
 //#define TTCN3_BUILDNUMBER 0  //0=R5x, 1=R5x01, 2=R5x02, ...
 
 /* The aggregated version number must be set manually since some stupid
@@ -38,7 +38,7 @@
  * TTCN3_VERSION = TTCN3_MAJOR * 1000000 + TTCN3_MINOR * 10000 +
  *                 TTCN3_PATCHLEVEL * 100 + TTCN3_BUILDNUMBER
  */
-#define TTCN3_VERSION 60300
+#define TTCN3_VERSION 60301
 
 /* A monotonically increasing version number.
  * An official release is deemed to have the highest possible build number (99)
