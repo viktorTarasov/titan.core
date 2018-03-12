d4e9a25eb10e294324e75bb9702d1bb9b3b675a0
Adrien Kirjak <adrien.kirjak@ericsson.com>
2018-03-12 09:12:54 +0100

    Added new tests from etsi and updated SoC document
    Signed-off-by: Adrien Kirjak <adrien.kirjak@ericsson.com>

---
+++ conformance_test/core_language_tests/positive_tests/16_functions_altsteps_testcases/1601_functions/160104_invoking_functions_from_specific_places/xf_NegSem_160104_invoking_functions_from_specific_places_245.cc
@@ -0,0 +1,17 @@
+/*****************************************************************
+ ** @author   STF 470
+ ** @version  0.0.1
+ ** @purpose  1:8.2.3.1, Verify that identifiers of parameter types are not imported together with external functions
+ ** @verdict  pass reject
+ *****************************************************************/
+
+#include "NegSem_160104_invoking_functions_from_specific_places_245.hh"
+
+namespace NegSem__160104__invoking__functions__from__specific__places__245
+{
+
+	BOOLEAN xf__NegSem__160104__invoking__functions__from__specific__places__245()
+    {
+		return true;
+	}
+}
